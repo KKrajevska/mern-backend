dabc5ac4f7c9c37029b7e3fda08f18591dd1facc
Kristina Krajevska <krajevska.kristina98@gmail.com>
2022-09-22 15:51:08 +0200

    wip

--- app.ts
+++ app.ts
@@ -10,10 +10,21 @@ require("dotenv").config();
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type,Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH");
+  next();
+});
+
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
-app.use((req, res, next): RequestHandler => {
+app.use((req, res, next) => {
   const error = new HttpError("Could not find this route.", 404);
   throw error;
 });
