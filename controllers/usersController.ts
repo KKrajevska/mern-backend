7f06b7e210f38724dae508bd920bf82958475a43
Kristina Krajevska <krajevska.kristina98@gmail.com>
2022-09-27 16:21:57 +0200

    jwt added

--- controllers/usersController.ts
+++ controllers/usersController.ts
@@ -3,6 +3,8 @@ import { UserT } from "../lib/types";
 import { HttpError } from "../models/httpError";
 import UserModel from "../models/user";
 import { validationResult } from "express-validator";
+import { compare, hash } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 export const getUsers: RequestHandler = async (req, res, next) => {
   let users;
@@ -45,10 +47,22 @@ export const signup: RequestHandler<{}, {}, UserT> = async (req, res, next) => {
     return next(error);
   }
 
+  let hashedPassword;
+
+  try {
+    hashedPassword = await hash(password, 12);
+  } catch (err) {
+    const error = new HttpError(
+      "Could not create user, please try again later",
+      500
+    );
+    return next(error);
+  }
+
   const createdUser = new UserModel({
     name,
     email,
-    password,
+    password: hashedPassword,
     image: req.file && req.file.path,
     places,
   });
@@ -59,7 +73,23 @@ export const signup: RequestHandler<{}, {}, UserT> = async (req, res, next) => {
     const error = new HttpError("Signup failed, please try again later", 500);
     return next(error);
   }
-  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+
+  let token;
+
+  try {
+    token = sign(
+      { userId: createdUser.id, email: createdUser.email },
+      "supersecret_dont_share",
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new HttpError("Signup failed, please try again later", 500);
+    return next(error);
+  }
+
+  res
+    .status(201)
+    .json({ userId: createdUser.id, email: createdUser.email, token: token });
 };
 
 export const login: RequestHandler<
@@ -81,7 +111,7 @@ export const login: RequestHandler<
     return next(error);
   }
 
-  if (!existingUser || existingUser.password !== password) {
+  if (!existingUser) {
     const error = new HttpError(
       "Invalid credentials, could not log you in.",
       401
@@ -89,8 +119,42 @@ export const login: RequestHandler<
     return next(error);
   }
 
+  let isValidPassword = false;
+
+  try {
+    isValidPassword = await compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError(
+      "Could not log you in, please check your credentials",
+      500
+    );
+    return next(error);
+  }
+
+  if (isValidPassword) {
+    const error = new HttpError(
+      "Invalid credentials, could not log you in.",
+      401
+    );
+    return next(error);
+  }
+
+  let token;
+
+  try {
+    token = sign(
+      { userId: existingUser.id, email: existingUser.email },
+      "supersecret_dont_share",
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new HttpError("Signup failed, please try again later", 500);
+    return next(error);
+  }
+
   res.json({
-    message: "Logged in!",
-    user: existingUser.toObject({ getters: true }),
+    userId: existingUser.id,
+    email: existingUser.email,
+    token: token,
   });
 };
